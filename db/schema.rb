8f57b2ed246cf9b8e2842f2090f9b16219a3a017
missopi <missopi@me.com>
2024-02-01 14:31:33 +0000

    Scaffold posts model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_142316) do
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+ActiveRecord::Schema[7.1].define(version: 0) do
 end
