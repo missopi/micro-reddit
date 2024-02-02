53f2fc935e3ffebd90bf04f9cb7ec6e1e1494352
missopi <missopi@me.com>
2024-02-02 15:30:24 +0000

    Add username column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_01_163953) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_151324) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_01_163953) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
