a030a2957f91139722eb8a759b9861132dfb840b
missopi <missopi@me.com>
2024-02-03 15:31:52 +0000

    Add and migrate user_id to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_155342) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_03_153018) do
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_02_155342) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
