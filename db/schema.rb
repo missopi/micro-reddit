5d9998f3ec69e23b2931936b3b3a058556b96e6f
missopi <missopi@me.com>
2024-02-03 15:19:20 +0000

    Add and migrate comments model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_02_151324) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_02_155342) do
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -31,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_02_151324) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
 end
