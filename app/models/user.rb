12f614c030edd442fa94a5950ac273a9f9bd0a29
missopi <missopi@me.com>
2024-02-09 14:57:40 +0000

    Add associations for comments and add user_id to comment table
    Migrate migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
+  has_many :comments
 
   validates :email, presence: true
   validates :username, presence: true
