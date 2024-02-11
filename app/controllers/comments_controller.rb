93438791b1daeecd27c184dc9529635a04e3b58a
missopi <missopi@me.com>
2024-02-11 15:00:25 +0000

    Link comment to user and post so that username can be visible on comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,22 @@
 
 # controller for comments
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+  before_action :current_user, only: %i[create destroy]
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:body))
-    redirect_to post_path(@post)
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+    respond_to do |format|
+      if @comment.save!
+        format.html { redirect_to @post, notice: 'Post was successfully commented.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { redirect_to @post, alert: 'post not commented' }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -18,6 +30,14 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :post_id)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:id])
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,11 @@
 
 # comment class
 class Comment < ApplicationRecord
+  belongs_to :user
   belongs_to :post
+  validates :user_id, presence: true
+  validates :body, presence: true, length: { maximum: 300 }
+  validates :post_id, presence: true
+
+  default_scope -> { order(created_at: :desc) }
 end
